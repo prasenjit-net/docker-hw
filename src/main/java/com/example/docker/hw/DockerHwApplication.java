ca48fec512b38b8f6057dc3953d87443f54b8b28
prasenjit-net <github@prasenjit.net>
2016-11-19 23:33:42 +0530

    docker added

--- src/main/java/com/example/docker/hw/DockerHwApplication.java
+++ src/main/java/com/example/docker/hw/DockerHwApplication.java
@@ -2,11 +2,19 @@ package com.example.docker.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DockerHwApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerHwApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home(){
+		return "Hello Docker World!";
 	}
 }
