b0965984a25023f13481cce29675d9f73a2cda07
prasenjit-net <github@prasenjit.net>
2016-11-19 23:04:57 +0530

    Initial Commit

---
+++ src/main/java/com/example/docker/hw/DockerHwApplication.java
@@ -0,0 +1,12 @@
+package com.example.docker.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerHwApplication.class, args);
+	}
+}
